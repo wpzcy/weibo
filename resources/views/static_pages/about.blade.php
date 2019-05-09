c611224839d6c8b4db7b6e43f5685fbfaaa14bf8
wpzcy <1085161374@qq.com>
2019-05-09 07:54:22 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts/default")
+@section("title","关于")
+@section("content")
+    关于页
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts/default")
+@section("title","帮助")
+@section("content")
+帮助页
+@stop
